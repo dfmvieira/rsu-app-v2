4a5c2c0a1a1603f5a706bcb0e8557cf7d2a100c5
Spakoin <valverde.s.d@gmail.com>
2021-06-17 14:50:20 +0100

    form

--- resources/js/views/viennaSigns/ViennaSigns.vue
+++ resources/js/views/viennaSigns/ViennaSigns.vue
@@ -34,23 +34,54 @@
                 <CCollapse :show="details.includes(index)">
                     <CCardBody>
                         
-                        <CMedia :aside-image-props="{src:item.image, height: 102 }">
+                        <CMedia :aside-image-props="{src:item.image, height: 120 ,width: 120}">
                         <h4>
                             {{item.name}}
                         </h4>
-                        <p class="text-muted">User since: {{item.registered}}</p>
-                        <CButton size="sm" color="info" class="">
+                        <p class="text-muted">Type: {{item.category}}</p>
+                        <!-- <CButton size="sm" color="info" class="">
                             User Settings
-                        </CButton>
-                        <CButton size="sm" color="danger" class="ml-1">
+                        </CButton> -->
+                        <CButton size="sm" color="danger" class="ml-1" data-toggle="modal" data-target="#exampleModalCenter">
                             Delete
                         </CButton>
+                        <CButton type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                            modal
+                        </CButton>
+                            <!-- Button trigger modal -->
+                            
+
+                            
                         </CMedia>
                     </CCardBody>
                 </CCollapse>
             </template>
             </CDataTable>
         </CCardBody>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+            Launch demo modal
+        </button>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    
     </CCard>
 </template>
 
