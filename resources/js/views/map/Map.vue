79702e8470ac6f7b190f51e6eb8eca1523df9723
Daniel Vieira <daniel5msn@gmail.com>
2021-07-06 10:19:13 +0100

    Draggable toolbox

--- resources/js/views/map/Map.vue
+++ resources/js/views/map/Map.vue
@@ -16,7 +16,7 @@
             <GmapInfoWindow :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen" @closeclick="infoWinOpen=false">
                 <CLink :href="infoLink" target="_blank">{{infoContent}}</CLink>
             </GmapInfoWindow>
-            <GmapMarker
+            <!-- <GmapMarker
                 :key="index"
                 v-for="(m, index) in markers"
                 :position="m.position"
@@ -25,9 +25,15 @@
                 :clickable="true"
                 :draggable="m.draggable"
                 @click="toggleInfoWindow(m, index)"
-            />
+            /> -->
             </GmapMap>
         </CCardBody>
+
+        <div ref="draggableContainer" id="toolbox">
+            <div id="toolboxHeader" @mousedown="dragMouseDown">
+                Toolbox
+            </div>
+        </div>
     </CCard>
 </template>
 
@@ -44,18 +50,80 @@ Vue.use(VueGoogleMaps, {
     // v: 'OPTIONAL VERSION NUMBER',
     // libraries: 'places', //// If you need to use place input
   }
-})
+});
 
 
 export default {
     name: 'Map',
-    data: () => {
+    data () {
         return {
             center:{lat: 39.7541724, lng: -8.8759984},
+            infoContent: '',
+            infoLink: '',
+            infoWindowPos: {
+                lat: 0,
+                lng: 0
+            },
+            infoOptions: {
+                pixelOffset: {
+                    width: 0,
+                    height: -35
+                }
+            },
+            infoWinOpen: false,
+            currentMidx: null,
+            toolbox: {
+                positions: {
+                    clientX: undefined,
+                    clientY: undefined,
+                    movementX: 0,
+                    movementY: 0
+                }
+            }
+            
         }
     },
     methods: {
-        
+        dragMouseDown: function (event) {
+            event.preventDefault();
+            // get the mouse cursor position at startup:
+            this.toolbox.positions.clientX = event.clientX
+            this.toolbox.positions.clientY = event.clientY
+            document.onmousemove = this.elementDrag
+            document.onmouseup = this.closeDragElement
+        },
+        elementDrag: function (event) {
+            event.preventDefault()
+            this.toolbox.positions.movementX = this.toolbox.positions.clientX - event.clientX
+            this.toolbox.positions.movementY = this.toolbox.positions.clientY - event.clientY
+            this.toolbox.positions.clientX = event.clientX
+            this.toolbox.positions.clientY = event.clientY
+            // set the element's new position:
+            this.$refs.draggableContainer.style.top = (this.$refs.draggableContainer.offsetTop - this.toolbox.positions.movementY) + 'px'
+            this.$refs.draggableContainer.style.left = (this.$refs.draggableContainer.offsetLeft - this.toolbox.positions.movementX) + 'px'
+        },
+        closeDragElement () {
+            document.onmouseup = null
+            document.onmousemove = null
+        }
     }
 }
 </script>
+
+<style scoped>
+    #toolbox {
+        position: absolute;
+        z-index: 9;
+        text-align: center;
+        background-color: #20202a;
+        border: 1px #000;
+    }
+
+    #toolboxHeader {
+        padding: 10px;
+        cursor: move;
+        z-index: 10;
+        background-color: #23242d;
+        border: 1px #000;
+    }
+</style>
