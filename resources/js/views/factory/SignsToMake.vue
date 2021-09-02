0f4603031e2d3da0c8a73fe753d1c04cd2cebbff
Daniel Vieira <daniel5msn@gmail.com>
2021-09-02 17:43:05 +0100

    Controller created

---
+++ resources/js/views/factory/SignsToMake.vue
@@ -0,0 +1,23 @@
+<template>
+    
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+    name: "SignsToMake",
+    data() {
+        return {
+            
+        signs: []
+        }
+    },
+    methods: {
+        getSignsToMake() {
+            axios.get('/api/factory')
+        }
+    }
+}
+</script>
