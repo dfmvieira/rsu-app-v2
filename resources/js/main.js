9aa8d91a40cabb8cab2c38471732ecb40769ce71
Daniel Vieira <daniel5msn@gmail.com>
2021-09-06 18:23:50 +0100

    Fixed api link bug

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,7 +6,7 @@ import CoreuiVue from '@coreui/vue'
 import { iconsSet as icons } from '../assets/icons/icons.js'
 import store from './store'
 
-Vue.prototype.$apiAdress = 'http://localhost:80'
+Vue.prototype.$apiAdress = ''
 Vue.config.performance = true
 Vue.use(CoreuiVue)
 
